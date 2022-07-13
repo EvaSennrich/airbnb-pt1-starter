35cebab11d3e03b0a16285a77001f03e9d68c3fb
Eva Sennrich <evasennrich@gmail.com>
2022-07-13 14:35:20 -0700

    added test application on app.tes.js

--- kavholm-express-api/app.test.js
+++ kavholm-express-api/app.test.js
@@ -1,19 +1,31 @@
-const request = require("supertest")
-const app = require("./app")
-const db = require("./db")
+const request = require("supertest");
+const app = require("./app");
+const db = require("./db");
 
 test("not found for site 404", async () => {
-  const resp = await request(app).get("/no-such-path")
-  expect(resp.statusCode).toEqual(404)
-})
+  const resp = await request(app).get("/no-such-path");
+  expect(resp.statusCode).toEqual(404);
+});
 
 test("not found for site 404 (test stack print)", async () => {
-  process.env.NODE_ENV = ""
-  const resp = await request(app).get("/no-such-path")
-  expect(resp.statusCode).toEqual(404)
-  delete process.env.NODE_ENV
-})
+  process.env.NODE_ENV = "";
+  const resp = await request(app).get("/no-such-path");
+  expect(resp.statusCode).toEqual(404);
+  delete process.env.NODE_ENV;
+});
 
 afterAll(async () => {
-  await db.end()
-})
+  await db.end();
+});
+
+describe("Test application", () => {
+  test("Not Found for site 404", async () => {
+    const res = await request(app).get("/wrong-endpoint");
+    expect(res.statusCode).toEqual(404);
+  });
+
+  test("Health check route returns valid response", async () => {
+    const res = await request(app).get("/");
+    expect(res.body).toEqual({ ping: "pong" });
+  });
+});
